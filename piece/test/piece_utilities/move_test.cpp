884d077a33525801580143f9c8e61db8eac1c677
Krzysztof Koch <loczek3603@gmail.com>
2023-11-14 21:37:04 +0100

    Add remaining Move tests

--- piece/test/piece_utilities/move_test.cpp
+++ piece/test/piece_utilities/move_test.cpp
@@ -49,4 +49,52 @@ TEST(MoveTest, IsForward) {
       Move::isForward(Position{-4, -5}, Position{-7, -5}, Color::White));
 }
 
+TEST(MoveTest, IsBackward) {
+  EXPECT_FALSE(Move::isBackward(Position{0, 0}, Position{0, 0}, Color::Black));
+  EXPECT_FALSE(Move::isBackward(Position{0, 0}, Position{0, 0}, Color::White));
+  EXPECT_FALSE(Move::isBackward(Position{0, 0}, Position{1, 0}, Color::Black));
+  EXPECT_TRUE(Move::isBackward(Position{0, 0}, Position{1, 0}, Color::White));
+  EXPECT_TRUE(Move::isBackward(Position{0, 0}, Position{-1, 0}, Color::Black));
+  EXPECT_FALSE(Move::isBackward(Position{0, 0}, Position{-1, 0}, Color::White));
+  EXPECT_FALSE(Move::isBackward(Position{0, 0}, Position{1, 3}, Color::Black));
+  EXPECT_FALSE(
+      Move::isBackward(Position{0, 0}, Position{-1, -2}, Color::White));
+  EXPECT_FALSE(Move::isBackward(Position{4, 5}, Position{7, 5}, Color::Black));
+  EXPECT_FALSE(Move::isBackward(Position{4, 5}, Position{1, 5}, Color::White));
+  EXPECT_TRUE(Move::isBackward(Position{-1, 5}, Position{-3, 5}, Color::Black));
+  EXPECT_TRUE(Move::isBackward(Position{4, 5}, Position{20, 5}, Color::White));
+}
+
+TEST(MoveTest, IsSide) {
+  EXPECT_FALSE(Move::isSide(Position{0, 0}, Position{0, 0}));
+  EXPECT_FALSE(Move::isSide(Position{0, 0}, Position{1, 1}));
+  EXPECT_FALSE(Move::isSide(Position{0, 0}, Position{-1, -1}));
+  EXPECT_FALSE(Move::isSide(Position{0, 0}, Position{1, -1}));
+  EXPECT_FALSE(Move::isSide(Position{0, 0}, Position{-1, 1}));
+  EXPECT_TRUE(Move::isSide(Position{0, 0}, Position{0, 1}));
+  EXPECT_TRUE(Move::isSide(Position{0, 0}, Position{0, -1}));
+  EXPECT_FALSE(Move::isSide(Position{0, 0}, Position{1, 0}));
+  EXPECT_FALSE(Move::isSide(Position{0, 0}, Position{-1, 0}));
+  EXPECT_TRUE(Move::isSide(Position{3, 1}, Position{3, 0}));
+  EXPECT_TRUE(Move::isSide(Position{3, 1}, Position{3, 7}));
+}
+
+TEST(MoveTest, IsByOne) {
+  EXPECT_FALSE(Move::isByOne(Position{0, 0}, Position{0, 0}));
+  EXPECT_TRUE(Move::isByOne(Position{0, 0}, Position{1, 1}));
+  EXPECT_TRUE(Move::isByOne(Position{0, 0}, Position{-1, -1}));
+  EXPECT_TRUE(Move::isByOne(Position{0, 0}, Position{1, -1}));
+  EXPECT_TRUE(Move::isByOne(Position{0, 0}, Position{-1, 1}));
+  EXPECT_TRUE(Move::isByOne(Position{0, 0}, Position{0, 1}));
+  EXPECT_TRUE(Move::isByOne(Position{0, 0}, Position{0, -1}));
+  EXPECT_TRUE(Move::isByOne(Position{0, 0}, Position{1, 0}));
+  EXPECT_TRUE(Move::isByOne(Position{0, 0}, Position{-1, 0}));
+  EXPECT_FALSE(Move::isByOne(Position{2, 5}, Position{0, 7}));
+  EXPECT_FALSE(Move::isByOne(Position{2, 5}, Position{4, 3}));
+  EXPECT_FALSE(Move::isByOne(Position{2, 5}, Position{0, 6}));
+  EXPECT_FALSE(Move::isByOne(Position{2, 5}, Position{4, 0}));
+  EXPECT_FALSE(Move::isByOne(Position{-30, 5}, Position{-32, 5}));
+  EXPECT_FALSE(Move::isByOne(Position{2, 5}, Position{2, 3}));
+}
+
 // TODO: Add remaining Move class tests
