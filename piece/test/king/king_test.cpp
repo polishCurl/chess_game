7d88522c38e975fdfbc94bef8182a8b7bcaa2464
Krzysztof Koch <loczek3603@gmail.com>
2023-11-18 14:47:42 +0100

    Finish King class unit tests

--- piece/test/king/king_test.cpp
+++ piece/test/king/king_test.cpp
@@ -6,41 +6,78 @@ using namespace chess::piece;
 
 TEST(KingTest, Constructor) {
   EXPECT_NO_THROW((King{Position{0, 0}, Color::Black}));
-  EXPECT_NO_THROW((King{std::string("D3"), Color::White}));
-  EXPECT_NO_THROW((King{std::string("H1"), Color::Black}));
-}
-
-TEST(KingTest, GetPosition) {
-  const King king{std::string("H1"), Color::Black};
-  EXPECT_EQ(king.getPosition(), (Position{"H1"}));
+  EXPECT_NO_THROW((King{Position{"D3"}, Color::White}));
+  EXPECT_NO_THROW((King{Position{"H1"}, Color::Black}));
 }
 
 class KingColorFixture : public ::testing::TestWithParam<Color> {};
 
+TEST_P(KingColorFixture, GetPosition) {
+  King king{Position{"H8"}, GetParam()};
+  EXPECT_EQ(king.getPosition(), Position{"H8"});
+  EXPECT_FALSE(king.move(Position{"H6"}));
+  EXPECT_EQ(king.getPosition(), Position{"H8"});
+  EXPECT_TRUE(king.move(Position{"G7"}));
+  EXPECT_EQ(king.getPosition(), Position{"G7"});
+}
+
 TEST_P(KingColorFixture, GetColor) {
-  const King king_black{std::string("H1"), GetParam()};
-  EXPECT_EQ(king_black.getColor(), GetParam());
+  const King king{Position{"H1"}, GetParam()};
+  EXPECT_EQ(king.getColor(), GetParam());
+}
+
+TEST_P(KingColorFixture, GetName) {
+  const King king{Position{"H1"}, GetParam()};
+  EXPECT_EQ(king.getName(), std::string("King"));
 }
 
 TEST_P(KingColorFixture, CanMove) {
-  const King king{std::string("E2"), GetParam()};
-  EXPECT_FALSE(king.canMove(std::string("E2")));
-  EXPECT_TRUE(king.canMove(std::string("E1")));
-  EXPECT_TRUE(king.canMove(std::string("E3")));
-  EXPECT_TRUE(king.canMove(std::string("D2")));
-  EXPECT_TRUE(king.canMove(std::string("F2")));
-  EXPECT_TRUE(king.canMove(std::string("D3")));
-  EXPECT_TRUE(king.canMove(std::string("F3")));
-  EXPECT_TRUE(king.canMove(std::string("D1")));
-  EXPECT_TRUE(king.canMove(std::string("F1")));
+  const King king{Position{"E2"}, GetParam()};
+  EXPECT_FALSE(king.canMove(Position{"E2"}));
+  EXPECT_TRUE(king.canMove(Position{"E1"}));
+  EXPECT_TRUE(king.canMove(Position{"E3"}));
+  EXPECT_TRUE(king.canMove(Position{"D2"}));
+  EXPECT_TRUE(king.canMove(Position{"F2"}));
+  EXPECT_TRUE(king.canMove(Position{"D3"}));
+  EXPECT_TRUE(king.canMove(Position{"F3"}));
+  EXPECT_TRUE(king.canMove(Position{"D1"}));
+  EXPECT_TRUE(king.canMove(Position{"F1"}));
 
-  EXPECT_FALSE(king.canMove(std::string("E4")));
-  EXPECT_FALSE(king.canMove(std::string("E8")));
-  EXPECT_FALSE(king.canMove(std::string("G2")));
-  EXPECT_FALSE(king.canMove(std::string("G3")));
-  EXPECT_FALSE(king.canMove(std::string("G1")));
-  EXPECT_FALSE(king.canMove(std::string("D4")));
-  EXPECT_FALSE(king.canMove(std::string("F4")));
+  EXPECT_FALSE(king.canMove(Position{"E4"}));
+  EXPECT_FALSE(king.canMove(Position{"E8"}));
+  EXPECT_FALSE(king.canMove(Position{"G2"}));
+  EXPECT_FALSE(king.canMove(Position{"G3"}));
+  EXPECT_FALSE(king.canMove(Position{"G1"}));
+  EXPECT_FALSE(king.canMove(Position{"D4"}));
+  EXPECT_FALSE(king.canMove(Position{"F4"}));
+}
+
+TEST_P(KingColorFixture, Move) {
+  EXPECT_FALSE((King{Position{"E2"}, GetParam()}).move(Position{"E2"}));
+  EXPECT_TRUE((King{Position{"E2"}, GetParam()}).move(Position{"E1"}));
+  EXPECT_TRUE((King{Position{"E2"}, GetParam()}).move(Position{"E3"}));
+  EXPECT_TRUE((King{Position{"E2"}, GetParam()}).move(Position{"D2"}));
+  EXPECT_TRUE((King{Position{"E2"}, GetParam()}).move(Position{"F2"}));
+  EXPECT_TRUE((King{Position{"E2"}, GetParam()}).move(Position{"D3"}));
+  EXPECT_TRUE((King{Position{"E2"}, GetParam()}).move(Position{"F3"}));
+  EXPECT_TRUE((King{Position{"E2"}, GetParam()}).move(Position{"D1"}));
+  EXPECT_TRUE((King{Position{"E2"}, GetParam()}).move(Position{"F1"}));
+
+  EXPECT_FALSE((King{Position{"E2"}, GetParam()}).move(Position{"E4"}));
+  EXPECT_FALSE((King{Position{"E2"}, GetParam()}).move(Position{"E8"}));
+  EXPECT_FALSE((King{Position{"E2"}, GetParam()}).move(Position{"G2"}));
+  EXPECT_FALSE((King{Position{"E2"}, GetParam()}).move(Position{"G3"}));
+  EXPECT_FALSE((King{Position{"E2"}, GetParam()}).move(Position{"G1"}));
+  EXPECT_FALSE((King{Position{"E2"}, GetParam()}).move(Position{"D4"}));
+  EXPECT_FALSE((King{Position{"E2"}, GetParam()}).move(Position{"F4"}));
+}
+
+TEST_P(KingColorFixture, HasBeenMoved) {
+  King king{Position{"H8"}, GetParam()};
+  EXPECT_FALSE(king.move(Position{"H6"}));
+  EXPECT_FALSE(king.hasBeenMoved());
+  EXPECT_TRUE(king.move(Position{"G7"}));
+  EXPECT_TRUE(king.hasBeenMoved());
 }
 
 INSTANTIATE_TEST_SUITE_P(KingTest, KingColorFixture,
